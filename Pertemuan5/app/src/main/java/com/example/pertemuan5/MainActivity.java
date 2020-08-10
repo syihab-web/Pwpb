4551eece92eea519610e3671f6813ed1d7eb5b24
syihab123-web <syihabudin234@gmail.com>
2020-08-10 10:07:14 +0700

    Pertemuan ke 5

--- Pertemuan5/app/src/main/java/com/example/pertemuan5/MainActivity.java
+++ Pertemuan5/app/src/main/java/com/example/pertemuan5/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pertemuan5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button next, next2;
+        next = (Button) findViewById(R.id.next);
+        next2 = (Button) findViewById(R.id.next2);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Framelayout();
+            }
+        });
+
+        next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Framelayout2();
+            }
+        });
+    }
+
+
+    //Method Pindah Halaman
+    public void Framelayout(){
+        Intent next = new Intent(this, Framelayout.class);
+        startActivity(next);
+    }
+    public void Framelayout2(){
+        Intent next2 = new Intent(this, FrameLayout2.class);
+        startActivity(next2);
     }
 }
